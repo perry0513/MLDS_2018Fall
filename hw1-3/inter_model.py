242231fb74e06be5e034dd4145a207005455ed27
perry0513 <perrypeiweichen@gmail.com>
2018-10-10 17:07:53 +0800

    produce better graph (200 epochs & space from 0.1 to 0.05)

--- hw1-3/inter_model.py
+++ hw1-3/inter_model.py
@@ -5,7 +5,7 @@ import tensorflow.keras.utils as np_utils
 import tensorflow.keras.datasets.mnist as mnist
 from matplotlib import pyplot as plt
 
-def load_data(train_size, batch_size):
+def load_data(train_size, batch_size, batched):
 	(x_train, y_train), (x_test, y_test) = mnist.load_data()
 	x_train = x_train[0:train_size]
 	y_train = y_train[0:train_size]
@@ -19,6 +19,9 @@ def load_data(train_size, batch_size):
 	y_train = np_utils.to_categorical(y_train, 10)
 	y_test = np_utils.to_categorical(y_test, 10)
 
+	if batched == False:
+		return (x_train, y_train), (x_test, y_test)
+
 	# batched (train_size % batch_size == 0)
 	train_data = list(zip( np.split(x_train, train_size/batch_size), np.split(y_train, train_size/batch_size) ))
 	test_data  = list(zip( np.split(x_test , train_size/batch_size), np.split(y_test , train_size/batch_size) ))
@@ -27,10 +30,10 @@ def load_data(train_size, batch_size):
 
 # Parameters
 train_size = 10000
-epochs = 100
+epochs = 200
 batch_size = [ 100, 1000 ]	# train_size % batch_size == 0
 display_step = 10
-alpha_list = np.arange(-1,2,0.1)
+alpha_list = np.arange(-1,2,0.05)
 
 # Network Parameters
 n_hidden_1 = 256 # 1st layer number of neurons
@@ -102,7 +105,7 @@ for i in range(2):
 	init = tf.global_variables_initializer()
 
 	# load training data
-	train_data, test_data = load_data(train_size, batch_size[i])
+	train_data, test_data = load_data(train_size, batch_size[i], True)
 
 	# Start training
 	with tf.Session() as sess:
@@ -125,7 +128,7 @@ for i in range(2):
 
 
 
-for alpha in alpha_list:
+for alpha in tqdm(alpha_list):
 
 	inter_weights = {
 		'h1': tf.Variable(np.array(trained_model[0][0][0]) * alpha + np.array(trained_model[1][0][0]) * (1-alpha)),
@@ -164,33 +167,18 @@ for alpha in alpha_list:
 	# Initialize the variables (i.e. assign their default value)
 	init = tf.global_variables_initializer()
 
-	# load testing data
-	train_data, test_data = load_data(train_size, batch_size[0]) # batch_size = 100
-	dataset = list(zip(train_data, test_data))
-
 	# Start testing
 	with tf.Session() as sess:
 		sess.run(init)
+		# Calculate training/testing loss/acc
+		(x_train, y_train), (x_test, y_test) = load_data(train_size, 0, False)
+		train_loss, train_acc = sess.run([loss_op, accuracy], feed_dict={ X: x_train, Y: y_train })
+		test_loss, test_acc = sess.run([loss_op, accuracy], feed_dict={ X: x_test, Y: y_test })
 
-		# Test model
-		avg_train_loss, avg_train_acc = 0, 0
-		avg_test_loss, avg_test_acc = 0, 0
-		for i, ((batch_x_train, batch_y_train), (batch_x_test, batch_y_test)) in enumerate(dataset):
-			# Calculate loss & accuracy
-			train_loss, train_acc = sess.run([loss_op, accuracy], feed_dict={ X: batch_x_train, Y: batch_y_train })
-			test_loss, test_acc = sess.run([loss_op, accuracy], feed_dict={ X: batch_x_test, Y: batch_y_test })
-
-			avg_train_loss += train_loss/train_size
-			avg_train_acc += train_acc/(train_size//batch_size[0])
-			avg_test_loss += test_loss/train_size
-			avg_test_acc += test_acc/(train_size//batch_size[0])
-
-		print("Alpha: {:>5} | Train Loss: {:.6f} \t| Train Acc: {:.6f} | Test Loss: {:.6f} | Test Acc: {:.6f}"
-			  .format(alpha, avg_train_loss, avg_train_acc, avg_test_loss, avg_test_acc))
-		inter_train_loss.append(avg_train_loss)
-		inter_train_acc.append(avg_train_acc)
-		inter_test_loss.append(avg_test_loss)
-		inter_test_acc.append(avg_test_acc)
+		inter_train_loss.append(train_loss/train_size)
+		inter_train_acc.append(train_acc)
+		inter_test_loss.append(test_loss/train_size)
+		inter_test_acc.append(test_acc)
 
 
 # plot
@@ -209,7 +197,7 @@ ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 color = 'tab:red'
 ax2.set_ylabel('accuracy', color=color)  # already handled the x-label with ax1
 ax2.plot(alpha_list, inter_train_acc, color=color, label='train')
-ax2.plot(alpha_list, inter_test_acc, color=color, linestyle='--', label='test')
+ax2.plot(alpha_list, inter_test_acc, color=color, label='test', linestyle='--')
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
