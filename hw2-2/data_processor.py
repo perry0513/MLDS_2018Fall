4e420b45ec9a4b6f0cc733b27ce602b551632f40
perry0513 <perrypeiweichen@gmail.com>
2018-11-15 00:34:27 +0800

    debugging

--- hw2-2/data_processor.py
+++ hw2-2/data_processor.py
@@ -29,7 +29,21 @@ class DataProcessor():
 		total_data_num = len(self.question_list)
 		zipped = list(zip(self.encoder_inputs, self.encoder_inputs_length, self.decoder_inputs, self.decoder_targets, self.decoder_targets_length))
 		np.random.shuffle(zipped)
-		shuffled_encoder_inputs, shuffled_encoder_inputs_length, shuffled_decoder_inputs, shuffled_decoder_targets, shuffled_decoder_targets_length = [ np.array(tup) for tup in zip(*zipped) ]
+		shuffled_encoder_inputs, shuffled_encoder_inputs_length, shuffled_decoder_inputs, \
+		shuffled_decoder_targets, shuffled_decoder_targets_length = [ np.array(tup) for tup in zip(*zipped) ]
+
+		# zipped = list(zip(range(total_data_num), self.encoder_inputs, self.decoder_inputs, self.decoder_targets))
+		# np.random.shuffle(zipped)
+		# shuffled_idx, shuffled_encoder_inputs, shuffled_decoder_inputs, shuffled_decoder_targets = [ np.array(tup) for tup in zip(*zipped) ]
+		# shuffled_encoder_inputs_length = [ len(self.question_list[idx]) for idx in shuffled_idx ]
+		# shuffled_decoder_targets_length = [ len(self.answer_list[idx]) + 1 for idx in shuffled_idx ]
+
+		# shuffled_encoder_inputs = [ np.array(v) for v in shuffled_encoder_inputs ]
+		# shuffled_decoder_inputs = [ np.array(v) for v in shuffled_decoder_inputs ]
+		# shuffled_decoder_targets = [ np.array(v) for v in shuffled_decoder_targets ]
+		# shuffled_encoder_inputs_length = [ np.array(v) for v in shuffled_encoder_inputs_length ]
+		# shuffled_decoder_targets_length = [ np.array(v) for v in shuffled_decoder_targets_length ]
+
 
 		num_of_batch = total_data_num // batch_size
 		batched_encoder_inputs  = np.split(np.array(shuffled_encoder_inputs[: num_of_batch*batch_size ]), num_of_batch)
@@ -51,13 +65,20 @@ class DataProcessor():
 			encoded_line = encoded_line + [self.PAD]*(self.max_seq_length-len(encoded_line))
 			self.encoder_inputs.append(np.array(encoded_line))
 
-		for line in self.answer_list:
+		for i, line in enumerate(self.answer_list):
 			encoded_line = []
 			for word in line:
 				idx = self.word2idx_dictionary.get(word, False)
 				encoded_line.append(idx if idx else self.UNK)
 			self.decoder_inputs.append(np.array([self.BOS] + encoded_line + [self.PAD]*(self.max_seq_length-len(encoded_line))))
 			self.decoder_targets.append(np.array(encoded_line + [self.EOS] + [self.PAD]*(self.max_seq_length-len(encoded_line))))
+
+			if self.decoder_inputs[-1].shape[0] != 28 or self.decoder_targets[-1].shape[0] != 28:
+				print("======== ERROR ========")
+				print(self.max_seq_length)
+				print(self.decoder_inputs[-1].shape[0])
+				print(self.decoder_targets[-1].shape[0])
+
 
 
 	def build_dictionary(self):
@@ -116,6 +137,14 @@ class DataProcessor():
 					self.answer_list.append(line.split(' '))
 
 ### DEBUG CODE ###
+# def check_same_length(arr):
+# 	length = arr[0].shape[0]
+# 	for v in arr:
+# 		if v.shape[0] is not length:
+# 			print("False")
+# 			return
+# 	print("True")
+
 # dp = DataProcessor()
 # print (len(dp.idx2word_dictionary))
 # batched_encoder_inputs, batched_encoder_inputs_length, batched_decoder_inputs, batched_decoder_targets, batched_decoder_targets_length = dp.get_batch(25)
@@ -124,3 +153,9 @@ class DataProcessor():
 # print(np.array(batched_decoder_inputs).shape)
 # print(np.array(batched_decoder_targets).shape)
 # print(np.array(batched_decoder_targets_length).shape)
+
+# check_same_length(batched_encoder_inputs[0])
+# check_same_length(batched_encoder_inputs_length)
+# check_same_length(batched_decoder_inputs[0])
+# check_same_length(batched_decoder_targets[0])
+# check_same_length(batched_decoder_targets_length)
