45016b98e4c6a4eac64192b738d4a5f17cccda4a
perry0513 <perrypeiweichen@gmail.com>
2018-11-16 15:24:12 +0800

    update infer.sh & trim.py

--- hw2-2/infer.sh
+++ hw2-2/infer.sh
@@ -1,5 +1,6 @@
 python3 infer.py
+python3 trim.py
 cd MLDS_hw2_2_data/evaluation/
-python3 main.py ../test_input.txt ../test_output.txt
+python3 main.py ../no_space_input.txt ../no_space_output.txt
 cd ../../
 
