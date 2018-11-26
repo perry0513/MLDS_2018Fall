c6c26ca42b28385040ff71d71fb985bc0ecc6d22
perry0513 <perrypeiweichen@gmail.com>
2018-11-27 01:18:23 +0800

    faster loading data

--- hw3-1/data_processor.py
+++ hw3-1/data_processor.py
@@ -1,24 +1,34 @@
 import numpy as np
+import os
 import cv2
 
 class DataProcessor():
 	def __init__(self, image_num=33431):
 		# Constant Parameters
 		self.image_num = image_num
-		self.image_path = "./AnimeDataset/faces/"
+		self.image_path = './AnimeDataset/faces/'
+		self.npy_path = './AnimeDataset/faces.npy'
 		self.image_list = []
 		self.load_data()
 
-
 	def load_data(self):
-		print ("Loading image data . . .")
-		image_list = []
-		for i in range(self.image_num):
-			image_list.append(cv2.resize(cv2.imread(self.image_path+str(i)+".jpg"), (64, 64)))
-		image_list = np.array(image_list)
+		print ('Loading image data . . .')
+		if os.path.isfile(self.npy_path):
+			self.image_list = np.load(self.npy_path)
+		else:
+			image_list = []
+			for i in range(self.image_num):
+				print('Loading ', i+1, ' . . .')
+				image_list.append(cv2.resize(cv2.imread(self.image_path+str(i)+".jpg"), (64, 64)))
+			image_list = np.array(image_list)
+			
+			# normalize
+			self.image_list = image_list / 256
+			# save as .npy file
+			np.save(self.npy_path, self.image_list)
 
-		# normalize
-		self.image_list = image_list / 256
+		print (self.image_list.shape)
+		print (self.image_list[0])
 
 
 	def get_batch(self, batch_size, d_iter):
@@ -30,7 +40,6 @@ class DataProcessor():
 
 		return batched_img[:d_iter] # batched_label
 
-# dp = DataProcessor()
-# dp.load_data()
+dp = DataProcessor()
 # a = dp.get_batch(100)
 # print(a[0].shape)
