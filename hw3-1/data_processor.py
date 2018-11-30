f9d15265d903744927a618108e2b3a8b1f183537
perry0513 <perrypeiweichen@gmail.com>
2018-11-30 15:17:00 +0800

    update files

--- hw3-1/data_processor.py
+++ hw3-1/data_processor.py
@@ -1,45 +1,60 @@
 import numpy as np
 import os
 import cv2
+from tqdm import tqdm
+from matplotlib import pyplot as plt
 
 class DataProcessor():
-	def __init__(self, image_num=33431):
+	def __init__(self, image_num=36739):
 		# Constant Parameters
 		self.image_num = image_num
-		self.image_path = './AnimeDataset/faces/'
-		self.npy_path = './AnimeDataset/faces.npy'
+		self.image_path = './AnimeDataset/extra_data/images/'
+		self.npy_path = './AnimeDataset/extra_faces.npy'
 		self.image_list = []
 		self.load_data()
 
 	def load_data(self):
 		print ('Loading image data . . .')
+		
 		if os.path.isfile(self.npy_path):
 			self.image_list = np.load(self.npy_path)
 		else:
 			image_list = []
-			for i in range(self.image_num):
-				print('Loading ', i+1, ' . . .')
-				image_list.append(cv2.resize(cv2.imread(self.image_path+str(i)+".jpg"), (64, 64)))
+			for i in tqdm(range(self.image_num)):
+# 				print('Loading ', i+1, ' . . .')
+				image_bgr = cv2.resize(cv2.imread(self.image_path+str(i)+".jpg"), (64, 64))
+				image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+				image_list.append(image_rgb)
+
 			image_list = np.array(image_list)
-			
-			# normalize
-			self.image_list = image_list / 256
+
+			# normalize (-1 ,1)
+			self.image_list = image_list / 128 - 1
 			# save as .npy file
 			np.save(self.npy_path, self.image_list)
 
 		# print (self.image_list.shape)
 		# print (self.image_list[0])
+		# cv2.imshow('img', self.image_list[1])
+		# cv2.waitKey()
+		# plt.imshow(self.image_list[1])
+		# plt.show()
 
 
-	def get_batch(self, batch_size, d_iter):
-		np.random.shuffle(self.image_list)
+	def get_batch(self, batch_size, batch_needed):
+		index = np.arange(self.image_num)
+		np.random.shuffle(index)
+		images = np.array([ self.image_list[idx] for idx in index ])
 		batch_num = self.image_num // batch_size
 
-		batched_img = np.split(self.image_list[: batch_size*batch_num ], batch_num)
+		batched_img = np.split(images[: batch_size*batch_num ], batch_num)
 		# batched_label = np.array([ np.ones(batch_size) for i in range(batch_num) ])
 
-		return batched_img[:d_iter] # batched_label
+		return batched_img # batched_label
 
 # dp = DataProcessor()
-# a = dp.get_batch(100)
+# a = dp.get_batch(100, 3)
 # print(a[0].shape)
+# print(a[0][0][0][0])
+# print(a[1][0][0][0])
+# print(a[2][0][0][0])
--- hw3-1/main.py
+++ hw3-1/main.py
@@ -1,14 +1,31 @@
 import numpy as np
 import tensorflow as tf
 from wgan import WGAN
+import os
+import argparse
 
-epochs = 5000
-batch_size = 64
+parser = argparse.ArgumentParser(description='tensorflow implementation of WGAN')
+parser.add_argument('-mode', type=str, default='train', help='train or infer mode')
+parser.add_argument('-curr_epoch', type=int, default='0', help='loads model trained for curr_epoch epochs, 0 for untrained')
+parser.add_argument('-epoch', type=int, help='epochs to train')
+args = parser.parse_args()
+mode = getattr(args, 'mode')
+curr_epoch = getattr(args, 'curr_epoch')
+epochs = getattr(args, 'epoch')
+
+batch_size = 128
 noise_dim = 128
 g_iter = 1
-d_iter = 5
+d_iter = 1
+
+# change curr_epoch, 0 for untrained
+# curr_epoch = 100
+restore_model_dir = './model/model_'+str(curr_epoch)+'/'
+print(restore_model_dir)
 
 model = WGAN(noise_dim)
-model.train(epochs, batch_size, g_iter, d_iter)
 
-
+if mode == 'train':
+	model.train(curr_epoch, epochs, batch_size, g_iter, d_iter, restore_model_dir)
+if mode == 'infer':
+	model.infer(restore_model_dir)
