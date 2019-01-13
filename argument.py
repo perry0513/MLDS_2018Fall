6b5e1dc2a2752047ab691b815f5a46fd70afd23f
Morris Lin <snoopy90338@gmail.com>
2019-01-13 19:03:41 +0800

    add dqn type argument

--- argument.py
+++ argument.py
@@ -8,10 +8,12 @@ def add_arguments(parser):
         parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for training')
     '''
     parser.add_argument('--batch_size', type=int, default=1, help='batch size for training')
-    parser.add_argument('--learning_rate', type=float, default=1e-4, help='learning rate for training')
+    parser.add_argument('--learning_rate', type=float, default=1.5e-4, help='learning rate for training')
     parser.add_argument('--episodes', type=int, default=5000, help='episodes for training')
     parser.add_argument('--save_history_period', type=int, default=10, help='save current reward in training per period')
     parser.add_argument('--load_checkpoint', action='store_true')
     parser.add_argument('--render', action='store_true')
+    parser.add_argument('--without_double_dqn', action='store_false')
+    parser.add_argument('--without_duel_dqn', action='store_false')
     parser.add_argument('--discount_factor', type=float, default=0.99)
     return parser
