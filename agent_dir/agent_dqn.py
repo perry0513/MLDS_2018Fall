0fbb50c99ed77c438a725de1887726cdf3e46458
andyh0913 <andyh0913@gmail.com>
2019-01-13 17:11:21 +0800

    revert checkpoints

--- agent_dir/agent_dqn.py
+++ agent_dir/agent_dqn.py
@@ -3,8 +3,6 @@ import numpy as np
 import tensorflow as tf
 import os
 
-from agent_dir.DQNModel import DQNModel
-
 class Agent_DQN(Agent):
 	def __init__(self, env, args):
 		"""
@@ -13,8 +11,6 @@ class Agent_DQN(Agent):
 		"""
 
 		super(Agent_DQN,self).__init__(env)
-
-        self.sess = tf.InteractiveSession()
 
 		if args.test_dqn:
 			#you can load your model here
@@ -29,9 +25,12 @@ class Agent_DQN(Agent):
 
 		self.action_size = self.env.action_space.n
 
+<<<<<<< HEAD
         self.model = DQNModel('model', self.action_size, True)
         self.target_model = DQNModel('target_model', self.action_size, True)
 
+=======
+>>>>>>> parent of 70549a9... modify DQNModel.py
 		self.checkpoints_dir = './checkpoints'
 		self.checkpoint_file = os.path.join(self.checkpoints_dir, 'dqn.ckpt')
 
